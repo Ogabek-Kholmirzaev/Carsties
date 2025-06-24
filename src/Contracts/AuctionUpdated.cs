3ac188aa2869510174c67e9e61983e4c8a9410b1
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-06-24 21:21:55 +0500

    Add AuctionCreated, AuctionUpdated, and AuctionDeleted contract classes

---
+++ src/Contracts/AuctionUpdated.cs
@@ -0,0 +1,11 @@
+﻿namespace Contracts;
+
+public class AuctionUpdated
+{
+    public string Id { get; set; } = null!;
+    public string Make { get; set; } = null!;
+    public string Model { get; set; } = null!;
+    public int Year { get; set; }
+    public string Color { get; set; } = null!;
+    public int Mileage { get; set; }
+}
