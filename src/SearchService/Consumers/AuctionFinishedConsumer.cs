50657fd9715d6ef6d699812c98c020fc78d98d82
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-06-28 02:56:02 +0500

    Add consumers to handle `AuctionFinished` and `BidPlaced` events in `SearchService` with structured logging and MongoDB updates

---
+++ src/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer(ILogger<AuctionFinishedConsumer> logger) : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        logger.LogInformation($"--> Consuming auction finished {context.Message.AuctionId}");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+        if (auction == null)
+        {
+            logger.LogError($"--> Auction {context.Message.AuctionId} not found");
+            return;
+        }
+        
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount ?? 0;
+        }
+        
+        auction.Status = "Finished";
+        await auction.SaveAsync();
+    }
+}
---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer(ILogger<BidPlacedConsumer> logger) : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        logger.LogInformation("--> Consuming bid placed: " + context.Message.Id);
+        
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+        if (auction == null)
+        {
+            logger.LogError($"--> Auction {context.Message.AuctionId} not found");
+            return;
+        }
+
+        if (context.Message.BidStatus.Contains("Accepted") && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await auction.SaveAsync();
+        }
+    }
+}
