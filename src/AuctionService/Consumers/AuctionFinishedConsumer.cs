63f7917a8d0d4b14408e216d17198a769eb749ee
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-06-28 01:34:16 +0500

    Add `AuctionFinishedConsumer` to handle auction completion events in `AuctionService`

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer(
+    AuctionDbContext dbContext,
+    ILogger<AuctionFinished> logger) 
+    : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        if (auction == null)
+        {
+            logger.LogError($"--> Auction {context.Message.AuctionId} not found");
+            return;
+        }
+        
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice
+            ? EStatus.Finished
+            : EStatus.ReserveNotMet;
+
+        await dbContext.SaveChangesAsync();
+    }
+}
