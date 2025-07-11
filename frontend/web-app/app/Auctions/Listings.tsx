d89437650317e7cfb6ab65c56aa32366cf6e4a18
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-07-12 02:38:24 +0500

    Installed Flowbite package and add pagination component

--- frontend/web-app/app/Auctions/Listings.tsx
+++ frontend/web-app/app/Auctions/Listings.tsx
@@ -1,8 +1,9 @@
 import { Auction, PagedResult } from "@/types";
 import AuctionCard from "./AuctionCard";
+import AppPagination from "../components/AppPagination";
 
 async function getDataAsync(): Promise<PagedResult<Auction>> {
-    const response = await fetch('http://localhost:6001/search?pageSize=10');
+    const response = await fetch('http://localhost:6001/search');
     if (!response.ok) {
         throw new Error('Failed to fetch data');
     }
@@ -14,10 +15,15 @@ export default async function Listings() {
     const data = await getDataAsync();
 
     return (
-        <div className="grid grid-cols-4 gap-6">
-            {data && data.results.map((auction => (
-                <AuctionCard key={auction.id} auction={auction} />
-            )))}
-        </div>
+        <>
+            <div className="grid grid-cols-4 gap-6">
+                {data && data.results.map((auction => (
+                    <AuctionCard key={auction.id} auction={auction} />
+                )))}
+            </div>
+            <div className="flex justify-center mt-4">
+                <AppPagination currentPage={1} pageCount={data.pageCount} />
+            </div>
+        </>
     )
 }
