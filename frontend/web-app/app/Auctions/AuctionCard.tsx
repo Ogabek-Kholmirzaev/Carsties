924e564c654c3ae1aa30e6493321159a71a1fa7b
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-07-11 23:46:16 +0500

    Implement AuctionCard and CountdownTimer components, update Listings to display auction data, and adjust ESLint configuration

---
+++ frontend/web-app/app/Auctions/AuctionCard.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import CountdownTimer from "./CountdownTimer";
+
+type Props = {
+    auction: any;
+}
+
+export default function AuctionCard({auction}: Props) {
+    return (
+        <a href="#">
+            <div className="relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden">
+                <Image
+                    src={auction.imageUrl}
+                    alt="Image of car"
+                    fill
+                    className="object-cover"
+                    priority
+                    sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 25vw"
+                />
+                <div className="absolute bottom-2 left-2">
+                    <CountdownTimer auctionEnd={auction.auctionEnd} />
+                </div>
+            </div>
+            <div className="flex justify-between items-center mt-4">
+                <h3 className="text-gray-700">{auction.make} {auction.model}</h3>
+                <p className="font-semibold text-sm"> {auction.year} </p>
+            </div>
+        </a>
+    )
+}
