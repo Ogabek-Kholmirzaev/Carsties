9c2e497765a45b87dcd7a926e111e8ed658c66b3
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-07-12 23:41:50 +0500

    Add Filters component and update Listings to support dynamic page size

--- frontend/web-app/app/Auctions/Listings.tsx
+++ frontend/web-app/app/Auctions/Listings.tsx
@@ -5,18 +5,20 @@ import AppPagination from "../components/AppPagination";
 import { useEffect, useState } from "react";
 import { Auction } from "@/types";
 import { getDataAsync } from "../actions/auctionActions";
+import Filters from "./Filters";
 
 export default function Listings() {
     const [auctions, setAuctions] = useState<Auction[]>([]);
     const [pageCount, setPageCount] = useState(0);
     const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(4);
 
     useEffect(() => {
-        getDataAsync(pageNumber).then(data => {
+        getDataAsync(pageNumber, pageSize).then(data => {
             setAuctions(data.results);
             setPageCount(data.pageCount);
         })
-    }, [pageNumber]);
+    }, [pageNumber, pageSize]);
 
     if (auctions.length === 0) {
         return <h3>Loading...</h3>
@@ -24,6 +26,7 @@ export default function Listings() {
 
     return (
         <>
+            <Filters pageSize={pageSize} setPageSize={setPageSize} />
             <div className="grid grid-cols-4 gap-6">
                 {auctions.map(auction => (
                     <AuctionCard key={auction.id} auction={auction} />
--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,8 +2,8 @@
 
 import { Auction, PagedResult } from "@/types";
 
-export async function getDataAsync(pageNumber: number): Promise<PagedResult<Auction>> {
-    const response = await fetch(`http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`);
+export async function getDataAsync(pageNumber: number, pageSize: number): Promise<PagedResult<Auction>> {
+    const response = await fetch(`http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`);
     if (!response.ok) {
         throw new Error('Failed to fetch data');
     }
