e2ff746a00dc64bdc9345cfbd18859c551446067
ogabek-xolmirzayev_epam <ogabek_xolmirzayev@epam.com>
2025-08-02 18:32:26 +0500

    Add BidsController with PlaceBid action for bidding functionality

---
+++ src/BiddingService/Controllers/BidsController.cs
@@ -0,0 +1,63 @@
+using BiddingService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace BiddingService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BidsController : ControllerBase
+{
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Bid>> PlaceBid(string auctionId, int amount)
+    {
+        var auction = await DB.Find<Auction>().OneAsync(auctionId);
+        if (auction == null)
+        {
+            // TODO: check with auction service if it has this auction
+            return NotFound();
+        }
+
+        if (auction.Seller == User.Identity?.Name)
+        {
+            return BadRequest("You cannot bid on your own auction");
+        }
+
+        var bid = new Bid
+        {
+            Amount = amount,
+            AuctionId = auctionId,
+            Bidder = User.Identity?.Name ?? "",
+        };
+
+        if (auction.AuctionEnd < DateTime.UtcNow)
+        {
+            bid.BidStatus = EBidStatus.Finished;
+        }
+        else
+        {
+            var highBid = await DB.Find<Bid>()
+                .Match(b => b.AuctionId == auctionId)
+                .Sort(b => b.Descending(x => x.Amount))
+                .ExecuteFirstAsync();
+
+            if (highBid != null && amount > highBid.Amount || highBid == null)
+            {
+                bid.BidStatus = amount > auction.ReservePrice
+                    ? EBidStatus.Accepted
+                    : EBidStatus.AcceptedBelowReserve;
+            }
+
+            if (highBid != null && bid.Amount <= highBid.Amount)
+            {
+                bid.BidStatus = EBidStatus.TooLow;
+            }
+        }
+
+        await DB.SaveAsync(bid);
+
+        return Ok(bid);
+    }
+}
